0993c38b667141bbd3dc97933545f97afce6cb6e
Luke Job <luke.job@westpac.co.nz>
2018-01-31 17:00:54 +1300

    Initial Commit

---
+++ Youtube Shuffle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Youtube Shuffle
+//
+//  Created by Luke Job on 31/01/18.
+//  Copyright © 2018 Luke Job. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
